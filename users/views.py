53b3a180ec0652a3a23b7be042f8ac463f037e18
jimmylin825 <joe79924@hotmail.com>
2025-07-06 19:18:18 +0800

    修正註冊頁

--- users/views.py
+++ users/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login, logout
 from .forms import RegisterForm
 from django.shortcuts import redirect
+from django.contrib import messages
 # Create your views here.
 
 def register_view(request):
@@ -9,11 +10,14 @@ def register_view(request):
         form = RegisterForm(request.POST)
         if form.is_valid():
             user = form.save()
-            login(request, user)  # 註冊後自動登入
+            login(request, user)
             return redirect('product_list')
+        else:
+            print(form.errors)  # 調試用
+            messages.error(request, '註冊失敗，請檢查輸入內容')
     else:
         form = RegisterForm()
-    return render(request, 'users/register.html', {'form': form, 'error' : '發生錯誤請重新嘗試'})
+    return render(request, 'users/register.html', {'form': form})
 
 
 def login_view(request):
